694f7b7f176478e89b4bfd3739000251ae75f0ac
WinnietheDEV <WorldWhyWin@gmail.com>
2023-02-09 20:10:17 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// button elements
+const decreaseBtn = document.querySelector(".decrease");
+const increaseBtn = document.querySelector(".increase");
+const submitBtn = document.querySelector(".submit");
+// number input element
+let input = document.querySelector(".number");
+// random number
+let secretNumber = Math.floor(Math.random() * 20 + 1);
+// status text
+let statusText = document.querySelector(".status");
+
+//button functions
+decreaseBtn.addEventListener("click", () => {
+  console.log(input);
+  input.value = Number(input.value) - 1;
+});
+increaseBtn.addEventListener("click", () => {
+  console.log(input);
+  input.value = Number(input.value) + 1;
+});
+submitBtn.addEventListener("click", () => {
+  const inputNumber = Number(input.value);
+  if (inputNumber === secretNumber) {
+    statusText.textContent = "correct";
+  } else if (inputNumber < secretNumber) {
+    statusText.textContent = "too low";
+  } else if (inputNumber > secretNumber) {
+    statusText.textContent = "too high";
+  }
+  console.log(statusText);
+});
+
+console.log(decreaseBtn);
